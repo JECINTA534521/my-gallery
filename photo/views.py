32d22356847dc85b9642978085ffacc7493e6d5a
jecinta <jecintawanjirug@gmail.com>
2020-02-27 12:35:59 +0300

    fixing the welcome function

--- photo/views.py
+++ photo/views.py
@@ -9,31 +9,31 @@ def welcome(request):
     location_results = Location.objects.all()
     return render(request,'welcome.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
 
-# def search_results(request):
+def search_results(request):
     
-#     if 'searchItem' in request.GET and request.GET["searchItem"]:
-#         search_term = request.GET.get("searchItem")
-#         searched_image = Image.search_by_category(search_term)
-#         message = f"{search_term}"
+    if 'searchItem' in request.GET and request.GET["searchItem"]:
+        search_term = request.GET.get("searchItem")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
 
-#         return render(request, 'search.html',{"message":message,"all_images": searched_image})
+        return render(request, 'search.html',{"message":message,"all_images": searched_image})
 
-#     else:
-#         message = "You haven't searched for any category"
-#         return render(request, 'search.html',{"message":message})
+    else:
+        message = "You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})
 
 
-# def get_category(request,category):
-#     category_results = Category.objects.all()
-#     location_results = Location.objects.all()
-#     category_result = Image.objects.filter(image_category__category_name = category)
-#     return render(request,'welcome.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
+def get_category(request,category):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Image.objects.filter(image_category__category_name = category)
+    return render(request,'welcome.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
 
-# def get_location(request,location):
-#     category_results = Category.objects.all()
-#     location_results = Location.objects.all()
-#     location_result = Image.objects.filter(image_location__location_name= location)
-#     return render(request,'welcome.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
+def get_location(request,location):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    location_result = Image.objects.filter(image_location__location_name= location)
+    return render(request,'welcome.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
 
 
 
