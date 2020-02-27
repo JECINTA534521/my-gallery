8c41a18fb50095aeb0178dc1c53ba0b59978967c
jecinta <jecintawanjirug@gmail.com>
2020-02-27 12:30:18 +0300

    creates a urls.py file and cretes the first url for the view function welcome

--- photo/urls.py
+++ photo/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
     url('^$', views.welcome,name = 'welcome'),
     url(r'^search/',views.search_results, name='search_results'),
     url(r'^category/(\w+)', views.get_category,name='get_category'),
-    url(r'^location/(\w+)', views.get_location,name='get_location'),
+    # url(r'^location/(\w+)', views.get_location,name='get_location'),
 ]
