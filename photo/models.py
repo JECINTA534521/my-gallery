e21f01a63a7db76e457d72b33f30868a168375fe
jecinta <jecintawanjirug@gmail.com>
2020-02-28 15:06:29 +0300

    this applies migrations to my application

--- photo/models.py
+++ photo/models.py
@@ -1,66 +1,64 @@
 from django.db import models
 
-# Create your models here.
-class Location(models.Model):
-    location_name = models.CharField(max_length=30) 
-    
-    
-    def __str__(self):
-        return self.location_name
-
-
-    def save_location(self):
-        self.save()
-
-
 class Category(models.Model):
-    category_name = models.CharField(max_length=40)
-
-    def __str__(self):
-        return self.category_name
-
+    name = models.CharField(max_length=30)
+    
     def save_category(self):
         self.save()
-
+        
+    def delete_category(self):
+        self.delete()
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Location(models.Model):
+    loc_name = models.CharField(max_length=30)
+    
+    def save_loc(self):
+        self.save()
+        
+    def delete_loc(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.loc_name
+    
 class Image(models.Model):
-    image_name = models.CharField(max_length =30)
-    image_description = models.TextField()
-    image_path = models.ImageField(upload_to = 'photos/')
-    image_location = models.ForeignKey(Location)
-    image_category = models.ForeignKey(Category)
-
+    image = models.ImageField(upload_to='photos/')
+    image_name = models.CharField(max_length=30)
+    image_descprition = models.CharField(max_length=150)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.get(id = id)
+        
+        return image
+    @classmethod
+    def search_image(cls,categorys):
+        images = cls.objects.filter(category__name = categorys)
+        
+        return images
+    
+    @classmethod
+    def filter_by_location(cls,location):
+        
+        images_locs = cls.objects.filter(location__loc_name=location)
+        
+        return images_locs
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+        
 
     def __str__(self):
         return self.image_name
-
-    @classmethod
-    def search_by_category(cls,search_term):
-        search_result = cls.objects.filter(image_category__category_name__icontains=search_term)
-        return search_result
-
-    @classmethod
-    def get_image_by_id(cls,incoming_id):
-        image_result = cls.objects.get(id=incoming_id)
-        return image_result
-
-    def save_image(self):
-        self.save()
-    def delete_image(self):
-        self.delete()
-
-    @classmethod
-    def retrieve_all(cls):
-        all_objects = Image.objects.all()
-        for item in all_objects:
-            return item
-
-    @classmethod
-    def update_image(cls,current_value,new_value):
-        fetched_object = Image.objects.filter(image_name=current_value).update(image_name=new_value)
-        return fetched_object
-
-    @classmethod
-    def filter_by_location(cls,location):
-        filtered_result = cls.objects.filter(image_location__location_name__icontains=location)
-        return filtered_result
-
+    class Meta:
+        ordering = ['image_name']
+   
--- photo/views.py
+++ photo/views.py
@@ -1,42 +1,24 @@
 from django.shortcuts import render
-from .models import Image,Category,Location
-#from django.http import HttpResponse
+from .models import Image
 
-# Create your views here.
-def welcome(request):
-    all_images = Image.objects.all()
-    category_results = Category.objects.all()
-    location_results = Location.objects.all()
-    return render(request,'welcome.html', {'all_images':all_images,'location_results':location_results,'category_results':category_results})
+def home(request):
+    images = Image.objects.all()
+    return render(request,'welcome.html',{"images":images})
 
 def search_results(request):
-    
-    if 'searchItem' in request.GET and request.GET["searchItem"]:
-        search_term = request.GET.get("searchItem")
-        searched_image = Image.search_by_category(search_term)
+    if 'image' in request.GET and request.GET["image"]:
+
+        search_term = request.GET.get("image")
+        searched_Images = Image.search_image(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"all_images": searched_image})
+        return render(request, 'search.html', {"message": message, "Images": searched_Images})
 
     else:
-        message = "You haven't searched for any category"
-        return render(request, 'search.html',{"message":message})
-
-
-def get_category(request,category):
-    category_results = Category.objects.all()
-    location_results = Location.objects.all()
-    category_result = Image.objects.filter(image_category__category_name = category)
-    return render(request,'welcome.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
-
-def get_location(request,location):
-    category_results = Category.objects.all()
-    location_results = Location.objects.all()
-    location_result = Image.objects.filter(image_location__location_name= location)
-    return render(request,'welcome.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
-
-
-
-
-   
+        message = "You haven't searched for any term "
+        return render(request, 'search.html', {"message": message})
     
+def search_location(request,location):
+    images_by_location = Image.filter_by_location(location)
+    
+    return render(request,'location.html', {"images":images_by_location})
