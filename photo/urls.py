afc1bc0543f47b12deea927f3f783ef0a0d0b3b6
jecinta <jecintawanjirug@gmail.com>
2020-02-28 15:01:22 +0300

    adds the search url and location url

--- photo/urls.py
+++ photo/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns=[
-    url('^$', views.welcome,name = 'welcome'),
+    url(r'^$',views.home,name='homePage'),
     url(r'^search/',views.search_results, name='search_results'),
-    url(r'^category/(\w+)', views.get_category,name='get_category'),
-    url(r'^location/(\w+)', views.get_location,name='get_location'),
+    url(r'^location/(\w+)',views.search_location,name='search_location')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
